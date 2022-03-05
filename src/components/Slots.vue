adb55d6870816b643f6eff059533f070b0ff1826
Glitch (titanium-pickle-orangutan) <none>
2022-03-05 02:35:11 +0000

    🪶👽 place available list relative to cursor direction

--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -1,8 +1,9 @@
 <template>
 <div>
-  <p>7</p>
-  <textarea rows="4" cols="20" v-model="availability" placeholder="Availability"></textarea>
-  <table>
+  <div :style="{left: 'calc(50vw - 90px)', top: this.top, position: 'fixed'}">
+    <textarea rows="4" cols="20" v-model="availability" placeholder="Availability" resize="none"></textarea>
+  </div>
+  <table @mouseleave="mouseLeave()">
     <tr>
       <th></th>
       <th v-for="day in dow" :key="day">{{day.charAt(0)}}</th>
@@ -10,7 +11,7 @@
     <tr v-for="row in 24" :key="row">
       <td>{{row}}</td>
       <td v-for="col in 7" :key="(row-1)*24+col"
-          @mouseover="mouseOver({row,col})">
+          @mouseover="mouseOver({row,col},$event)">
       </td>
     </tr>
   </table>
@@ -24,9 +25,18 @@
       userdata: Array, // [{name, slots},...]
     },
     methods: {
-      mouseOver(cell) {
+      mouseOver(cell, event) {
         const row = cell.row-1;
         const col = cell.col-1;
+        if(row != this.last_row) {
+          const dir = row > this.last_row;
+          if(dir) {
+            this.top = event.clientY - 100 +"px";
+          } else {
+            this.top = event.clientY +"px";
+          }
+          this.last_row = row;
+        }
         
         this.availability = [];
         for( let i = 0; i<this.userdata.length; i++){
@@ -36,7 +46,10 @@
           let avail = week[col];
           if(avail.available) this.availability.push(name);
         }
-        // console.log("day,hour,available:", this.dow[col], row, this.availability);
+      },
+      mouseLeave() {
+        this.top = "0px";
+        this.last_row = -1;
       },
     },
     data() {
@@ -44,13 +57,19 @@
       return {
         isMouseDown: false,
         dow: ["M","T","W","Th","F","Sa","Su"],
-        availability: []
+        availability: [],
+        left: 0,
+        top: 0,
+        last_row: -1,
       };
     }
   };
 </script>
 
 <style>
+  textarea {
+    border: 1 solid blue;
+  }
   table {
     border: 1px solid;
     border-collapse: collapse;
