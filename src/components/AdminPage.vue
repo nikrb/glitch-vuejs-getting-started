dd633408e101f9e0c202fd8c8f5ae7040f678aee
Glitch (titanium-pickle-orangutan) <none>
2022-03-06 02:06:53 +0000

    🕠🛖 added day/hour admin view

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <slots :userdata="userdata"></slots>
+    <button @click="toggleView()">toggle</button>
+    <div v-if="this.slotView">
+      <slot-pick :userdata="userdata"></slot-pick>
+    </div>
+    <div v-else>
+      <slots :userdata="userdata"></slots>
+    </div>
   </div>
 </template>
 
 <script>
-  import Slots from './Slots.vue'
+  import Slots from './Slots.vue';
+  import SlotPick from './SlotPick.vue';
   export default {
     name: "AdminPage",
     props: {
@@ -13,6 +20,17 @@
     },
     components: {
       Slots,
+      SlotPick,
+    },
+    methods: {
+      toggleView(){
+        this.slotView = !this.slotView;
+      },
+    },
+    data() {
+      return {
+        slotView: true,
+      };
     },
   };
 </script>
